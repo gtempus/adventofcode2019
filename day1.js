f9b511d8c3b457a8dccc4b43831aff514f34fb93
Gary Tempus <gary@heysparkbox.com>
2019-12-13 16:01:42 -0500

    refactor: ramda compose

--- day1.js
+++ day1.js
@@ -1,11 +1,15 @@
+const { compose } = require('ramda');
+
 const substract = (num) => (amount) => amount - num;
-
 const divideBy = (divisor) => (dividend) => dividend / divisor;
-
 const roundDown = (num) => Math.floor(num);
 
 const fuelCalculator = (mass) => (
-  substract(2)(roundDown(divideBy(3)(mass)))
+  compose(
+    substract(2),
+    roundDown,
+    divideBy(3),
+  )(mass)
 );
 
 module.exports = { fuelCalculator };
