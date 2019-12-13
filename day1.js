9d465cedd94c6b9905b5368303af6a1549e2c492
Gary Tempus <gary@heysparkbox.com>
2019-12-13 15:57:15 -0500

    feat: Day1 examples passing.

--- day1.js
+++ day1.js
@@ -1,3 +1,11 @@
-const fuelCalculator = () => 2;
+const substract = (num) => (amount) => amount - num;
+
+const divideBy = (divisor) => (dividend) => dividend / divisor;
+
+const roundDown = (num) => Math.floor(num);
+
+const fuelCalculator = (mass) => (
+  substract(2)(roundDown(divideBy(3)(mass)))
+);
 
 module.exports = { fuelCalculator };
