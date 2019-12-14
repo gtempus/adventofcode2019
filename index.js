c6e8a7cb4a8dc6201e8bea6fb740b1d147075b16
Gary Tempus <gary@heysparkbox.com>
2019-12-13 21:29:52 -0500

    style: Resolve linting errors.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 const dataDay1 = require('./data/day1.json');
 const { fuelCounterUpper } = require('./day1');
 
 console.log('⭐ Advent of Code 2019 ⭐');
-console.group('Day One⛽');
+console.group('Day One ⛽');
 console.log('Fuel Counter-Upper:', fuelCounterUpper(dataDay1.data));
