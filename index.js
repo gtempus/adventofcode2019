1fab9f276428148a7eee91c8c8202591bcd9761a
Gary Tempus <gary@heysparkbox.com>
2019-12-13 22:54:25 -0500

    refactor: destructure day1 data.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
-const dataDay1 = require('./data/day1.json');
+const { data: data1 } = require('./data/day1.json');
 const { fuelCounterUpper, fuelCounterUpperWithFuel } = require('./day1');
 
 console.log('⭐ Advent of Code 2019 ⭐');
 console.group('Day One ⛽');
-console.log('Fuel Counter-Upper:', fuelCounterUpper(dataDay1.data));
-console.log('Fuel Counter-Upper(w/fuel):', fuelCounterUpperWithFuel(dataDay1.data));
+console.log('Fuel Counter-Upper:', fuelCounterUpper(data1));
+console.log('Fuel Counter-Upper(w/fuel):', fuelCounterUpperWithFuel(data1));
