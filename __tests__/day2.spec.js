bd7956e397772eb59463d3eac47896fea4580e1d
Gary Tempus <gary@heysparkbox.com>
2019-12-14 15:53:06 -0500

    feat: Get some draft commands for program eval.

--- __tests__/day2.spec.js
+++ __tests__/day2.spec.js
@@ -1,5 +1,12 @@
 /* eslint-disable no-undef */
-const { nextInstruction } = require('../day2');
+const {
+  nextInstruction,
+  fetch,
+  addFn,
+  haltFn,
+  decode,
+  store,
+} = require('../day2');
 
 describe('Program Counter', () => {
   it('increments by 4 each time', () => {
@@ -10,3 +17,48 @@ describe('Program Counter', () => {
   });
 });
 
+describe('Fetch', () => {
+  it('fetches the requested value from the program', () => {
+    const fetchWithProgram = fetch([0, 4, 5, 1, 30]);
+    const result = fetchWithProgram(3);
+    expect(result).toBe(1);
+  });
+});
+
+describe('Decode', () => {
+  it('recognizes an ADD instruction', () => {
+    const result = decode(1);
+    expect(result).toBe(addFn);
+  });
+
+  it('recognized a HALT instruction', () => {
+    const result = decode(99);
+    expect(result).toBe(haltFn);
+  });
+});
+
+describe('Evaluate', () => {
+  describe('ADD', () => {
+    it('adds the two values', () => {
+      const addFnWithProgram = addFn([1, 2, 1, 0]);
+      const result = addFnWithProgram(0);
+      expect(result).toBe(3);
+    });
+  });
+
+  describe('HALT', () => {
+    it('is a no-op for now', () => {
+      const haltFnWithProgram = haltFn([1, 2, 1, 0]);
+      const result = haltFnWithProgram(0);
+      expect(result).toMatchObject({});
+    });
+  });
+
+  describe('Storing the result', () => {
+    it('stores the value at the given program position', () => {
+      const storeWithProgram = store([0, 1, 5, 0]);
+      const result = storeWithProgram(0, 20);
+      expect(result).toMatchObject([20, 1, 5, 0]);
+    });
+  });
+});
--- day2.js
+++ day2.js
@@ -1,7 +1,8 @@
+const { update } = require('ramda');
+
 /*
  * Roll over, Von Neumann!!!
  */
-
 const programCounter = (function* incrementBy4() {
   let index = 0;
   while (true) {
@@ -11,5 +12,24 @@ const programCounter = (function* incrementBy4() {
 }());
 
 const nextInstruction = () => programCounter.next().value;
+const fetch = (program) => (position) => program[position];
+const addFn = (program) => (instructionStart) => (
+  program[instructionStart + 1] + program[instructionStart + 2]
+);
+const haltFn = () => () => ({});
+const decode = (instruction) => (
+  /* eslint-disable no-nested-ternary */
+  instruction === 1 ? addFn : instruction === 99 ? haltFn : undefined
+);
+const store = (program) => (position, value) => (
+  update(position, value, program)
+);
 
-module.exports = { nextInstruction };
+module.exports = {
+  nextInstruction,
+  fetch,
+  addFn,
+  haltFn,
+  decode,
+  store,
+};
